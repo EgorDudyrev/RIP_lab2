fe2eb7a57b99a2340f08001cb5d56ffc7620b315
EgorDudyrev <egor.dudyrev@yandex.ru>
2017-09-17 20:53:32 +0300

    Completed

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-#from lab_classes import *
-import vk_api
+from lab_classes import *
+from matplotlib import pylab
 
-api = vk_api.VkApi().get_api()
+#user_id = VK_User('56337756').execute()
+name = input('Type username or id: ')
+print()
+user_id = None
+try:
+    user_id = VK_User(name).execute()
+except Exception as e:
+    print(e)
+    quit(1)
 
-print(api.users.get(user_ids='egordudyrev'))
+ages= None
+try:
+    ages = VK_Friends(user_id).execute()
+except Exception as e:
+    print(e)
+    quit(1)
 
+hist = [(x,ages.count(x)) for x in set(ages)]
+hist.sort(key=lambda x: x[0])
+for age, number in hist:
+    print(age,'#'*number)
 
-#response = vk_api.VkApi().method(method='users.get',values={'user_ids': 'egordudyrev'})
-#print(response)
+pylab.hist(ages,bins=((max(ages)-min(ages))))
+pylab.xlabel('age')
+pylab.xticks(range(0,max(ages),5),rotation=45)
+pylab.title('Distrubution of {}\'s friends ages'.format(name))
+pylab.show()
--- lab_classes.py
+++ lab_classes.py
@@ -1,22 +1,60 @@
 from base_client import *
 import requests
-import vk_api
+import json
+import datetime
 
 class VK_User(BaseClient):
     def __init__(self, username):
-        pass
+        self.name = username
 
-    BASE_URL = 'https://vk.com/dev/'
+    BASE_URL = 'https://api.vk.com/method/'
     method = 'users.get'
     http_method = 'GET'
 
     # Отправка запроса к VK API
     def _get_data(self, method, http_method):
-        response = None
-
-        # todo выполнить запрос
-        response = vk_api.VkApi.method(method='users.get',values={'user_ids': 'egordudyrev'})
-
+        url=self.generate_url(method)
+        response = requests.get(url=url+'?user_ids='+self.name)
         return self.response_handler(response)
+
+    def response_handler(self, response):
+        js = json.loads(response.text)
+        try:
+            return js['response'][0]['uid']
+        except:
+            raise Exception('There\'s no such user')
+
+
+class VK_Friends(BaseClient):
+    def __init__(self, user_id):
+        self.id = user_id
+
+    BASE_URL = 'https://api.vk.com/method/'
+    method = 'friends.get'
+    http_method = 'GET'
+
+    # Отправка запроса к VK API
+    def _get_data(self, method, http_method):
+        url=self.generate_url(method)
+        response = requests.get(url=url+'?user_id='+str(self.id)+'&fields=bdate')
+        return self.response_handler(response)
+
+    def response_handler(self, response):
+        js = json.loads(response.text)
+        try:
+            dates = []
+            for friend in js['response']:
+                try:
+                    dates.append(datetime.datetime.strptime(friend['bdate'],"%d.%m.%Y").date())
+                except:
+                    pass
+
+            ages = [int((datetime.date.today()-date).days/365.2425) for date in dates]
+            return ages
+
+        except KeyError:
+            return KeyError(js)
+
+
 
 
